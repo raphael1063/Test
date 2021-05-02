eeca7dde1e179e382f131f1a18ab28a5863737b4
Wonjin <wjlim2@sysofting.co.kr>
2021-05-02 23:36:37 +0900

    first commit

--- app/src/main/java/com/robin1/testapplication/MainActivity.kt
+++ app/src/main/java/com/robin1/testapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.robin1.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            finishAffinity()
+        }
     }
 }
